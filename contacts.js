585cb13e05c43eeee0464606864f86702f8f7aef
YevheniiaMushyk <142007023+YevheniiaMushyk@users.noreply.github.com>
2024-05-09 22:47:47 +0300

    add files

---
+++ contacts.js
@@ -0,0 +1,21 @@
+/*
+ * Створи змінну contactsPath і запиши в неї шлях до файлу contacts.json.
+ * Для складання шляху використовуй методи модуля path
+ * const contactsPath = ;
+ */
+
+async function listContacts() {
+	// ...твій код. Повертає масив контактів.
+}
+
+async function getContactById(contactId) {
+	// ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+async function removeContact(contactId) {
+	// ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+async function addContact(name, email, phone) {
+	// ...твій код. Повертає об'єкт доданого контакту (з id).
+}
